e3b6cfd1831bd6f8b3be2da1baa682c7bff3d688
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-20 20:39:43 +0000

    Encoding categorical data

--- ANN.py
+++ ANN.py
@@ -7,3 +7,12 @@ X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
 
 print(X)
+print(y)
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+print(X)
