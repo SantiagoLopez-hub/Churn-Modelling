1a0e4633c2623e43c80df0e7c6b5f783dd3665ba
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-21 20:10:35 +0000

    Feature scaling

--- ANN.py
+++ ANN.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 from sklearn.model_selection import train_test_split
 
+from sklearn.preprocessing import StandardScaler
 
 
 
@@ -31,3 +32,10 @@ print(X[:, :])
 
 # Splitting dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+print(y_test)
+
+
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
