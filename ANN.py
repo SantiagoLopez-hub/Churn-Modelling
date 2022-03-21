5242e620a017b1cd0e4e881942113e2a554b5b30
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-21 20:08:02 +0000

    Complete encoding categorical data

--- ANN.py
+++ ANN.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
@@ -11,8 +17,11 @@ print(y)
 
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
 
 print(X)
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X[:, :])
