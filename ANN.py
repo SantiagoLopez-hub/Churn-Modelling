d708bcf3af4612fd61cc4ee379648cdf783c7ef9
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-21 20:09:54 +0000

    Split dataset

--- ANN.py
+++ ANN.py
@@ -7,11 +7,14 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
+from sklearn.model_selection import train_test_split
+
+
+
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
-
 print(X)
 print(y)
 
@@ -19,9 +22,12 @@ print(y)
 # Encoding categorical data
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
-
 print(X)
 
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X[:, :])
+
+
+# Splitting dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
